a907e58625a6052cc03e7f4bff33bd5a48216751
Andrea Zavatta <zavattaandrea@gmail.com>
2023-01-17 15:08:54 +0100

    aggiunte classi per splittare le responsabilità della classe gioco

---
+++ Classes/StatoGioco.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class StatoGioco
+    {
+        public string IdGiocatoreMano { get; set; }
+        public bool Iniziato { get; set; }
+        public int Giri { get; set; }
+    }
+}
